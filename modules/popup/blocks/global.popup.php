8d88dc0abadadd28289784d794f4dd93211a97f0
Hồ Ngọc Triển <hongoctrien@2mit.org>
2018-09-27 13:41:03 +0700

    Sửa lỗi hiển thị popup theo cấu hình

--- modules/popup/blocks/global.popup.php
+++ modules/popup/blocks/global.popup.php
@@ -6,18 +6,17 @@
  * @Copyright (C) 2012 VINADES.,JSC. All rights reserved
  * @Createdate 2-10-2010 20:59
  */
-if (! defined('NV_SYSTEM'))
-    die('Stop!!!');
+if (!defined('NV_SYSTEM')) die('Stop!!!');
 
-if (! nv_function_exists('nv_popup')) {
+if (!nv_function_exists('nv_popup')) {
 
     function nv_popup($block_config)
     {
         global $global_config, $site_mods, $nv_Cache, $module_config;
-        
+
         $module = $block_config['module'];
         $row = $module_config[$module];
-        
+
         if ($row['active']) {
             if (file_exists(NV_ROOTDIR . "/themes/" . $global_config['module_theme'] . "/modules/" . $module . "/block.popup.tpl")) {
                 $block_theme = $global_config['module_theme'];
@@ -26,29 +25,31 @@ if (! nv_function_exists('nv_popup')) {
             } else {
                 $block_theme = "default";
             }
-            
+
             $popup_content_file = NV_ROOTDIR . '/' . NV_ASSETS_DIR . '/popup_content.txt';
-            if(file_exists($popup_content_file)){
+            if (file_exists($popup_content_file)) {
                 $row['popup_content'] = file_get_contents($popup_content_file);
-            }else{
+            } else {
                 return '';
             }
-            
+
             $row['timer_open'] = $row['timer_open'] * 1000;
-            
+            if (!empty($row['timer_close'])) {
+                $row['timer_close'] = $row['timer_close'] * 1000;
+            }
+
             $xtpl = new XTemplate("block.popup.tpl", NV_ROOTDIR . "/themes/" . $block_theme . "/modules/" . $module);
             $xtpl->assign('TEMPLATE', $block_theme);
             $xtpl->assign('ROW', $row);
-            
-            if ($row['timer_close']) {
-                $row['timer_close'] = $row['timer_close'] * 1000;
+
+            if (!empty($row['timer_close'])) {
                 $xtpl->parse('main.timer_close');
             }
-            
-            if($row['develop_mode']){
+
+            if ($row['develop_mode']) {
                 $xtpl->parse('main');
             }
-            
+
             $xtpl->parse('main');
             return $xtpl->text('main');
         }
