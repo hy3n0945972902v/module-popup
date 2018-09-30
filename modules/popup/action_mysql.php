820b83b56b2544ecb95f12cc6e2bf80e1ee63559
Hồ Ngọc Triển <hongoctrien@2mit.org>
2018-09-30 17:42:17 +0700

    Dùng plugin thay cho block để hiển thị popup

--- modules/popup/action_mysql.php
+++ modules/popup/action_mysql.php
@@ -16,8 +16,29 @@ $config = array(
     'timer_close' => 0,
     'size_w' => 600,
     'size_h' => 400,
-    'develop_mode' => 0
+    'develop_mode' => 0,
+    'all_func' => 1,
+    'funcid' => ''
 );
 foreach ($config as $config_name => $config_value) {
     $sql_create_module[] = "INSERT INTO " . NV_CONFIG_GLOBALTABLE . " (lang, module, config_name, config_value) VALUES ('" . $lang . "', " . $db->quote($module_name) . ", " . $db->quote($config_name) . ", " . $db->quote($config_value) . ")";
+}
+
+// thêm plugin
+$plugin_file = 'show_popup.php';
+$plugin_area = 4;
+
+$_sql = 'SELECT max(weight) FROM ' . $db_config['prefix'] . '_plugin WHERE plugin_area=' . $plugin_area;
+$weight = $db->query($_sql)->fetchColumn();
+$weight = intval($weight) + 1;
+
+try {
+    $sth = $db->prepare('INSERT INTO ' . $db_config['prefix'] . '_plugin (plugin_file, plugin_area, weight) VALUES (:plugin_file, :plugin_area, :weight)');
+    $sth->bindParam(':plugin_file', $plugin_file, PDO::PARAM_STR);
+    $sth->bindParam(':plugin_area', $plugin_area, PDO::PARAM_INT);
+    $sth->bindParam(':weight', $weight, PDO::PARAM_INT);
+    $sth->execute();
+    nv_save_file_config_global();
+} catch (PDOException $e) {
+    trigger_error($e->getMessage());
 }
--- modules/popup/admin/main.php
+++ modules/popup/admin/main.php
@@ -9,7 +9,8 @@
 if (!defined('NV_IS_FILE_ADMIN')) die('Stop!!!');
 
 $page_title = $lang_module['main'];
-$popup_content_file = NV_ROOTDIR . '/' . NV_ASSETS_DIR . '/popup_content.txt';
+$popup_content_file = NV_ROOTDIR . '/' . NV_FILES_DIR . '/popup_content.txt';
+$error = array();
 
 if ($nv_Request->isset_request('save', 'post')) {
     $popup['active'] = $nv_Request->get_bool('active', 'post', 0);
@@ -19,37 +20,58 @@ if ($nv_Request->isset_request('save', 'post')) {
     $popup['size_h'] = $nv_Request->get_int('size_h', 'post', 400);
     $popup['develop_mode'] = $nv_Request->get_int('develop_mode', 'post', 0);
     $popup['popup_content'] = $_POST['popup_content'];
-    
-    $sth = $db->prepare("UPDATE " . NV_CONFIG_GLOBALTABLE . " SET config_value = :config_value WHERE lang = '" . NV_LANG_DATA . "' AND module = :module_name AND config_name = :config_name");
-    $sth->bindParam(':module_name', $module_name, PDO::PARAM_STR);
-    foreach ($popup as $config_name => $config_value) {
-        $sth->bindParam(':config_name', $config_name, PDO::PARAM_STR);
-        $sth->bindParam(':config_value', $config_value, PDO::PARAM_STR);
-        $sth->execute();
-    }
-    
-    if (!empty($popup['popup_content'])) {
-        file_put_contents($popup_content_file, $popup['popup_content']);
-    }
-    
-    $nv_Cache->delMod('settings');
-    
-    Header("Location: " . NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name);
-    die();
-}
 
-$row = $module_config[$module_name];
+    $popup['all_func'] = $nv_Request->get_int('all_func', 'post', 1);
+    $popup['funcid'] = $nv_Request->get_array('func_id', 'post');
+
+    if (empty($popup['all_func']) and empty($popup['funcid'])) {
+        $error[] = $lang_module['block_no_func'];
+    } else {
+        $array_funcid_module = array();
+        foreach ($site_mods as $mod => $_arr_mod) {
+            foreach ($_arr_mod['funcs'] as $_func => $_row) {
+                if ($_row['show_func']) {
+                    $array_funcid_module[$_row['func_id']] = $mod;
+                }
+            }
+        }
+
+        if ($popup['all_func']) {
+            $popup['funcid'] = array_keys($array_funcid_module);
+        } else {
+            $popup['funcid'] = array_intersect($popup['funcid'], array_keys($array_funcid_module));
+        }
+        $popup['funcid'] = implode(',', $popup['funcid']);
+
+        $sth = $db->prepare("UPDATE " . NV_CONFIG_GLOBALTABLE . " SET config_value = :config_value WHERE lang = '" . NV_LANG_DATA . "' AND module = :module_name AND config_name = :config_name");
+        $sth->bindParam(':module_name', $module_name, PDO::PARAM_STR);
+        foreach ($popup as $config_name => $config_value) {
+            $sth->bindParam(':config_name', $config_name, PDO::PARAM_STR);
+            $sth->bindParam(':config_value', $config_value, PDO::PARAM_STR);
+            $sth->execute();
+        }
+
+        if (!empty($popup['popup_content'])) {
+            file_put_contents($popup_content_file, $popup['popup_content']);
+        }
+
+        $nv_Cache->delMod('settings');
+
+        Header("Location: " . NV_BASE_ADMINURL . 'index.php?' . NV_LANG_VARIABLE . '=' . NV_LANG_DATA . '&' . NV_NAME_VARIABLE . '=' . $module_name);
+        die();
+    }
+}
 
 if (file_exists($popup_content_file)) {
-    $row['popup_content'] = file_get_contents($popup_content_file);
+    $array_config['popup_content'] = file_get_contents($popup_content_file);
 } else {
-    $row['popup_content'] = '';
+    $array_config['popup_content'] = '';
 }
 
-$row['popup_content'] = nv_editor_br2nl($row['popup_content']);
+$array_config['popup_content'] = nv_editor_br2nl($array_config['popup_content']);
 
-if (!empty($row['popup_content'])) {
-    $row['popup_content'] = nv_htmlspecialchars($row['popup_content']);
+if (!empty($array_config['popup_content'])) {
+    $array_config['popup_content'] = nv_htmlspecialchars($array_config['popup_content']);
 }
 
 if (defined('NV_EDITOR')) {
@@ -57,18 +79,79 @@ if (defined('NV_EDITOR')) {
 }
 
 if (defined('NV_EDITOR') and nv_function_exists('nv_aleditor')) {
-    $row['popup_content'] = nv_aleditor("popup_content", '100%', '300px', $row['popup_content']);
+    $array_config['popup_content'] = nv_aleditor("popup_content", '100%', '300px', $array_config['popup_content']);
 } else {
-    $row['popup_content'] = "<textarea style=\"width:100%;height:300px\" name=\"popup_content\">" . $row['popup_content'] . "</textarea>";
+    $array_config['popup_content'] = "<textarea style=\"width:100%;height:300px\" name=\"popup_content\">" . $array_config['popup_content'] . "</textarea>";
 }
 
-$row['ck_active'] = $row['active'] ? 'checked="checked"' : '';
-$row['ck_develop_mode'] = $row['develop_mode'] ? 'checked="checked"' : '';
+$array_config['funcid'] = !empty($array_config['funcid']) ? explode(',', $array_config['funcid']) : array();
+
+$sql = 'SELECT func_id, func_custom_name, in_module FROM ' . NV_MODFUNCS_TABLE . ' WHERE show_func=1 ORDER BY in_module ASC, subweight ASC';
+$func_result = $db->query($sql);
+$aray_mod_func = array();
+while (list ($id_i, $func_custom_name_i, $in_module_i) = $func_result->fetch(3)) {
+    $aray_mod_func[$in_module_i][] = array(
+        'id' => $id_i,
+        'func_custom_name' => $func_custom_name_i
+    );
+}
+
+$array_config['ck_active'] = $array_config['active'] ? 'checked="checked"' : '';
+$array_config['ck_develop_mode'] = $array_config['develop_mode'] ? 'checked="checked"' : '';
 
 $xtpl = new XTemplate("main.tpl", NV_ROOTDIR . "/themes/" . $global_config['module_theme'] . "/modules/" . $module_file);
 $xtpl->assign('LANG', $lang_module);
 $xtpl->assign('ACTION', NV_BASE_ADMINURL . "index.php?" . NV_NAME_VARIABLE . "=" . $module_name);
-$xtpl->assign('DATA', $row);
+$xtpl->assign('DATA', $array_config);
+$xtpl->assign('SHOWS_ALL_FUNC', (intval($array_config['all_func'])) ? ' style="display:none" ' : '');
+
+$add_block_module = array(
+    1 => $lang_module['add_block_all_module'],
+    0 => $lang_module['add_block_select_module']
+);
+
+$i = 1;
+foreach ($add_block_module as $b_key => $b_value) {
+    $xtpl->assign('I', $i);
+    $xtpl->assign('B_KEY', $b_key);
+    $xtpl->assign('B_VALUE', $b_value);
+    $xtpl->assign('CK', ($array_config['all_func'] == $b_key) ? ' checked="checked"' : '');
+    $xtpl->parse('main.add_block_module');
+    $i++;
+}
+
+$sql = 'SELECT title, custom_title FROM ' . NV_MODULES_TABLE . ' ORDER BY weight ASC';
+$result = $db->query($sql);
+while (list ($m_title, $m_custom_title) = $result->fetch(3)) {
+    if (isset($aray_mod_func[$m_title]) and sizeof($aray_mod_func[$m_title]) > 0) {
+        $i = 0;
+        foreach ($aray_mod_func[$m_title] as $aray_mod_func_i) {
+
+            $sel = '';
+            if (in_array($aray_mod_func_i['id'], $array_config['funcid'])) {
+                ++$i;
+                $sel = ' checked="checked"';
+            }
+
+            $xtpl->assign('SELECTED', $sel);
+            $xtpl->assign('FUNCID', $aray_mod_func_i['id']);
+            $xtpl->assign('FUNCNAME', $aray_mod_func_i['func_custom_name']);
+
+            $xtpl->parse('main.loopfuncs.fuc');
+        }
+
+        $xtpl->assign('M_TITLE', $m_title);
+        $xtpl->assign('M_CUSTOM_TITLE', $m_custom_title);
+        $xtpl->assign('M_CHECKED', (sizeof($aray_mod_func[$m_title]) == $i) ? ' checked="checked"' : '');
+
+        $xtpl->parse('main.loopfuncs');
+    }
+}
+
+if (!empty($error)) {
+    $xtpl->assign('ERROR', implode('<br />', $error));
+    $xtpl->parse('main.error');
+}
 
 $xtpl->parse('main');
 $contents = $xtpl->text('main');
